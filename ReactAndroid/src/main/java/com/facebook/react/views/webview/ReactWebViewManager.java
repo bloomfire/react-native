4764a7711f4297c2f80c4a5ef19c6b2d37178dbe
Dennis Yao <dennis@bloomfire.com>
2016-10-13 15:55:38 -0500

    BF-11001 fix errors after upgrading to .32

--- ReactAndroid/src/main/java/com/facebook/react/views/webview/ReactWebViewManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/webview/ReactWebViewManager.java
@@ -65,6 +65,8 @@ import com.facebook.react.views.webview.events.TopMessageEvent;
 
 import org.json.JSONObject;
 import org.json.JSONException;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
 
 /**
  * Manages instances of {@link WebView}
@@ -140,18 +142,18 @@ public class ReactWebViewManager extends SimpleViewManager<WebView> {
               createWebViewEvent(webView, url)));
     }
 
-    @Override
-    public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        if (url.startsWith("http://") || url.startsWith("https://") ||
-            url.startsWith("file://")) {
-          return false;
-        } else {
-          Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-          intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-          view.getContext().startActivity(intent);
-          return true;
-        }
-    }
+    // @Override
+    // public boolean shouldOverrideUrlLoading(WebView view, String url) {
+    //     if (url.startsWith("http://") || url.startsWith("https://") ||
+    //         url.startsWith("file://")) {
+    //       return false;
+    //     } else {
+    //       Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+    //       intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    //       view.getContext().startActivity(intent);
+    //       return true;
+    //     }
+    // }
 
     @Override
     public void onReceivedError(
