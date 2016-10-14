3428c1bc61668177eae09c0affd471d3a45b7aa2
Dennis Yao <dennis@bloomfire.com>
2016-10-14 09:26:00 -0500

    BF-11001 incorporate default shouldOverrideUrlLoading

--- ReactAndroid/src/main/java/com/facebook/react/views/webview/ReactWebViewManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/webview/ReactWebViewManager.java
@@ -128,19 +128,6 @@ public class ReactWebViewManager extends SimpleViewManager<WebView> {
               createWebViewEvent(webView, url)));
     }
 
-    // @Override
-    // public boolean shouldOverrideUrlLoading(WebView view, String url) {
-    //     if (url.startsWith("http://") || url.startsWith("https://") ||
-    //         url.startsWith("file://")) {
-    //       return false;
-    //     } else {
-    //       Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-    //       intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    //       view.getContext().startActivity(intent);
-    //       return true;
-    //     }
-    // }
-
     @Override
     public void onReceivedError(
         WebView webView,
@@ -206,7 +193,16 @@ public class ReactWebViewManager extends SimpleViewManager<WebView> {
           return true;
       }
 
-      return false;
+
+      if (url.startsWith("http://") || url.startsWith("https://") ||
+          url.startsWith("file://")) {
+          return false;
+      } else {
+          Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+          intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+          view.getContext().startActivity(intent);
+          return true;
+      }
     }
 
 
